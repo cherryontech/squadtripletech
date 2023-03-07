96440350fc1de6e5b913cf8a5b82b299dfde3814
novellac <38117965+novellac@users.noreply.github.com>
2023-03-06 21:07:38 -0500

    feat: Add components to app for proof of concept

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '/src/assets/main.css'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
   <header>incoming header</header>
 
-  <main>this will be main area</main>
+  <main>
+    <StepInput
+      v-model:stepText="bio"
+      :stepNumber="1"
+      stepLabel="Step 1: Add your current pitch"
+    />
+
+    <StepInput
+      v-model:stepText="jobDescription"
+      :stepNumber="2"
+      stepLabel="Step 2: Paste a job description"
+    />
+
+    <BaseButton @click="createMelange">Generate</BaseButton>
+
+    <div class="border">
+      <h2>Melange list:</h2>
+      <ul>
+        <li class="m-2 border" v-for="(item, index) in melange" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+import BaseButton from './components/BaseButton.vue'
+import StepInput from './components/StepInput.vue'
+
+const bio = ref('')
+const jobDescription = ref('')
+
+const melange = ref([])
+const createMelange = () => {
+  melange.value.push(`${bio.value}, ${jobDescription.value}`)
+
+  jobDescription.value = ''
+}
+</script>
